b476dc11869063968203bd9fe37be68ce1ba1a5c
guoping <guoping@17guagua.com>
2019-08-27 18:19:21 +0800

    借鉴一下

--- app/src/main/java/com/gp/jswebview/MainActivity.java
+++ app/src/main/java/com/gp/jswebview/MainActivity.java
@@ -1,12 +1,16 @@
 package com.gp.jswebview;
 
 import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -27,13 +31,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    //调用无参无返回值
-                    String call = "javascript:readyToGo()";
+                    //调用js无参无返回值函数
+                    String call = "javascript:javaCallJS()";
                     myWebView.loadUrl(call);
                     return true;
                 case R.id.navigation_dashboard:
                     //调用有参无返回值
-                    String call1 = "javascript:alertMessage(\"" + "content" + "\")";
+                    String call1 = "javascript:javaCallJSWithArgs(\"" + "fromJava" + "\")";
                     myWebView.loadUrl(call1);
                     return true;
                 case R.id.navigation_notifications:
@@ -47,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    private void evaluateJavaScript(WebView webView,String ss){
+    private void evaluateJavaScript(WebView webView, String ss){
         webView.evaluateJavascript("getYourCar(\"" + ss + "\")", new ValueCallback<String>() {
             @Override
             public void onReceiveValue(String s) {
@@ -81,6 +85,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
+        });
+
+        myWebView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                final WebView.HitTestResult hitTestResult = myWebView.getHitTestResult();
+                // 如果是图片类型或者是带有图片链接的类型
+                if (hitTestResult.getType() == WebView.HitTestResult.IMAGE_TYPE ||
+                        hitTestResult.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
+                    // 弹出保存图片的对话框
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setItems(new String[]{"查看大图", "保存图片到相册"}, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    String picUrl = hitTestResult.getExtra();
+                                    //获取图片
+                                    Log.e("picUrl", picUrl);
+                                    switch (which) {
+                                        case 0:
+                                            break;
+                                        case 1:
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                }
+                            })
+                            .show();
+                    return true;
+                }
+                return false;
+            }
         });
 
         myWebView.loadUrl("file:///android_asset/test.html");
